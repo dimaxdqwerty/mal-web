0e44f9d96607f0311b37afbcd067a89e52ee03e2
Dzmitry Dubrova <dimaxdqwerty@gmail.com>
2022-07-03 21:13:30 +0300

    more improves for animeList page

--- models/Node.go
+++ models/Node.go
@@ -1,7 +1,38 @@
 package models
 
+import "time"
+
 type Node struct {
-	ID          int         `json:"id"`
-	Title       string      `json:"title"`
-	MainPicture MainPicture `json:"main_picture"`
+	ID                     int               `json:"id"`
+	Title                  string            `json:"title"`
+	MainPicture            MainPicture       `json:"main_picture"`
+	AlternativeTitles      AlternativeTitles `json:"alternative_titles"`
+	StartDate              string            `json:"start_date"`
+	EndDate                string            `json:"end_date"`
+	Synopsis               string            `json:"synopsis"`
+	Mean                   float64           `json:"mean"`
+	Rank                   int               `json:"rank"`
+	Popularity             int               `json:"popularity"`
+	NumListUsers           int               `json:"num_list_users"`
+	NumScoringUsers        int               `json:"num_scoring_users"`
+	Nsfw                   string            `json:"nsfw"`
+	CreatedAt              time.Time         `json:"created_at"`
+	UpdatedAt              time.Time         `json:"updated_at"`
+	MediaType              string            `json:"media_type"`
+	Status                 string            `json:"status"`
+	Genres                 []Genres          `json:"genres"`
+	MyListStatus           MyListStatus      `json:"my_list_status"`
+	NumEpisodes            int               `json:"num_episodes"`
+	StartSeason            StartSeason       `json:"start_season"`
+	Broadcast              Broadcast         `json:"broadcast"`
+	Source                 string            `json:"source"`
+	AverageEpisodeDuration int               `json:"average_episode_duration"`
+	Rating                 string            `json:"rating"`
+	Pictures               []Pictures        `json:"pictures"`
+	Background             string            `json:"background"`
+	RelatedAnime           []RelatedAnime    `json:"related_anime"`
+	//RelatedManga    		[]RelatedManga 		`json:"related_manga"` TODO: create RelatedManga struct
+	Recommendations []Recommendations `json:"recommendations"`
+	Studios         []Studios         `json:"studios"`
+	Statistics      Statistics        `json:"statistics"`
 }
--- operations/AnimeOperations.go
+++ operations/AnimeOperations.go
@@ -19,7 +19,7 @@ var (
 
 	GetAnimeQuery = "https://api.myanimelist.net/v2/anime"
 
-	Fields = "?fields=id,title,main_picture,alternative_titles,start_date,end_date," +
+	Fields = "fields=id,title,main_picture,alternative_titles,start_date,end_date," +
 		"synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at," +
 		"updated_at,media_type,status,genres,my_list_status,num_episodes,start_season," +
 		"broadcast,source,average_episode_duration,rating,pictures,background,related_anime," +
@@ -30,7 +30,7 @@ var anime models.Anime
 var animeList AnimeList
 
 func GetAnimeByID(ID string) models.Anime {
-	req, err := http.NewRequest("GET", GetAnimeQuery+"/"+ID+Fields, nil)
+	req, err := http.NewRequest("GET", GetAnimeQuery+"/"+ID+"?"+Fields, nil)
 	req.Header.Add("X-MAL-CLIENT-ID", MalClientID)
 	handleErr(err)
 
@@ -47,7 +47,7 @@ func GetAnimeByID(ID string) models.Anime {
 }
 
 func GetAnimeRankingList(limit string, offset string) AnimeList {
-	req, err := http.NewRequest("GET", GetAnimeQuery+"/ranking"+"?rankingType=all"+"&limit="+limit+"&offset="+offset+Fields, nil)
+	req, err := http.NewRequest("GET", GetAnimeQuery+"/ranking"+"?rankingType=all"+"&limit="+limit+"&offset="+offset+"&"+Fields, nil)
 	req.Header.Add("X-MAL-CLIENT-ID", MalClientID)
 	handleErr(err)
 
